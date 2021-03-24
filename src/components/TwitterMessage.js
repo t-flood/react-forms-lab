1c04a08616210d45ed0e732480a037c53a3e9e11
Tim Flood <tflood9@gmail.com>
2021-03-24 19:52:58 -0400

    commit tree 13c409418ffa580b06fd15649eeea6f440c6cd48

--- src/components/TwitterMessage.js
+++ src/components/TwitterMessage.js
@@ -4,14 +4,31 @@ class TwitterMessage extends React.Component {
   constructor() {
     super();
 
-    this.state = {};
+    this.state = {
+      value: "",
+    };
   }
+
+  handleChange = (event) => {
+    this.setState({
+      value: event.target.value,
+    });
+  };
 
   render() {
     return (
       <div>
         <strong>Your message:</strong>
-        <input type="text" name="message" id="message" />
+        <input
+          type="text"
+          name="message"
+          id="message"
+          value={this.state.value}
+          onChange={this.handleChange}
+        />
+        <p>
+          Remaining characters: {this.props.maxChars - this.state.value.length}
+        </p>
       </div>
     );
   }
